4895b45bbd817fbea8429057e11fbdda9e5ca781
gutierrezjdr <gutierrezjdr@gmail.com>
2019-04-05 11:58:21 -0400

    Fixing server issues

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 //DATABASE
-var db = pqp('postgres://username:password@host:port/database')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);jj
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
