049b4ad1939c62b1c2a92c44eda149abbb3970be
gutierrezjdr <gutierrezjdr@gmail.com>
2019-04-21 12:45:51 -0400

    Databse changed to new db info and gitignore added for node modules

--- models/db.js
+++ models/db.js
@@ -3,10 +3,10 @@ var mysql = require('mysql')
 
 
 var connection = mysql.createConnection({
-	host: 'inchatnitodb.cnjllkigqfjv.us-east-1.rds.amazonaws.com',
+	host: 'inchatnito.caypex3thbml.us-east-1.rds.amazonaws.com',
 	user: 'InchatnitoMaster',
-	password:'inchatnito',
-	database:'inchatnitodb'
+	password:'#1Inchatnito',
+	database:'inchatnito'
 });
 
 connection.connect((err)=>{
