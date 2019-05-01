0972be61c54bb481d3a503b0207cd4f29022d132
gutierrezjdr <gutierrezjdr@gmail.com>
2019-05-01 12:36:20 -0400

    Did npm run build

--- app.js
+++ app.js
@@ -22,13 +22,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client','build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
 app.use('/login',loginRouter);
 app.use('/courses',coursesRouter);
+
+app.get('/*',function(req, res, next) {
+	res.sendFile(path.join(__dirname,'client','build','index.html'));
+});
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
